7c2e5b41fa6c2ab53fbd3216e4ee6d402020a0c8
Mohammad Ismail <forwork.ismail@gmail.com>
2022-08-11 10:55:07 +0430

    feat: Create Springboot project with Spring Web and Spring Secuirty

---
+++ src/main/kotlin/com/github/forworkismail/springbootkotlinjwt/SpringbootKotlinJwtApplication.kt
@@ -0,0 +1,11 @@
+package com.github.forworkismail.springbootkotlinjwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinJwtApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinJwtApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/forworkismail/springbootkotlinjwt/SpringbootKotlinJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.forworkismail.springbootkotlinjwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinJwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
